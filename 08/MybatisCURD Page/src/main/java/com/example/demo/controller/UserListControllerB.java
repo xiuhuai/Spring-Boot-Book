87ee3b4b4f47b68a4d47f77fccc50da760f5e6ae
xiuhuai <363694485@qq.com>
2020-02-25 22:53:02 +0800

    Update UserListControllerB.java
    网友ygl提供了更严谨的写法，作者当时丢了默认值

--- 08/MybatisCURD Page/src/main/java/com/example/demo/controller/UserListControllerB.java
+++ 08/MybatisCURD Page/src/main/java/com/example/demo/controller/UserListControllerB.java
@@ -6,6 +6,7 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,7 +19,9 @@ public class UserListControllerB {
     UserMapper userMapper;
     //http://localhost:8080/listall2?pageNum=1&pageSize=2
     @RequestMapping("/listall2")
-    public Page<User> getUserList(Integer pageNum, Integer pageSize){
+    public Page<User> getUserList(@RequestParam(value="pageNum",defaultValue="0")int pageNum, @RequestParam(value = "pageSize", defaultValue = "5") int pageSize)
+    //public Page<User> getUserList(Integer pageNum, Integer pageSize)
+    {
         PageHelper.startPage(pageNum, pageSize);
         Page<User>  userList= userMapper.getUserList();
         return userList;
